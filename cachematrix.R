96f02434a324057befb3ec80e6f07df11e8d9cf1
AmalGupta <gupta.am@live.in>
2014-06-17 15:37:44 +0530

    Update cachematrix.R
    Adding the first draft. Two functions defined :-  makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 to cache the inverse of a matrix.
+## The programming assignment works with two functions 
+## * makeCacheMatrix < - which makes a special data structure to cache the inverse of a matrix and return it. It can
+##                       be viewed as method which helps creates the environment variable. 
+## Usage :- the following two lines let you know how to invoke the methods. 
+##              a<-makeCacheMatrix()   # create a cache matrix structure with default value
+##              a$set(matrix(1:4,2,2),2,2)     # Assign the matrix     
+##
+##  * cacheSolve() which validates whether the matrix entered is in the cache. If so, the output is displayed 
+## from the cache, otherwise, the output is calculated.
 
-## Write a short comment describing this function
-
+## The function returns a data structure from the entered matrix. It has the method set, get, setmatrix, getmatrix etc.
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This function solves for the inverse if the inverse is not already in the cache. Once the inverse is calculated,
+## it stores the data in the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        
+        matrix<-x$get()
+        m<-solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
